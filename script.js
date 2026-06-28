bf6bfec4bed85680dda4412e5ba0957336e94209
Satish Akkera <41571649+akkera911@users.noreply.github.com>
2026-06-28 19:17:00 +0530

    Implement live email preview update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById("name").addEventListener("input", function() {
     document.getElementById("preview-name").textContent = this.value;
 });
+document.getElementById("email").addEventListener("input", function() {
+    document.getElementById("preview-email").textContent = this.value;
+});
