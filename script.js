45fc77fd3384da984a4c978e2f91791d88860962
Satish Akkera <41571649+akkera911@users.noreply.github.com>
2026-06-28 22:33:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.getElementById("name").addEventListener("input", function() {
     document.getElementById("preview-name").textContent = this.value;
 });
+
 document.getElementById("email").addEventListener("input", function() {
     document.getElementById("preview-email").textContent = this.value;
 });
-document.getElementById("email").addEventListener("input", function() {
-    document.getElementById("preview-email").textContent = this.value;
-});
+
 document.getElementById("summary").addEventListener("input", function() {
     document.getElementById("preview-summary").textContent = this.value;
 });
